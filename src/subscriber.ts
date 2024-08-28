186065d187b6ee36d8169d196407cf610cb3cd9a
Akshay Petta <akshaypetta@gmail.com>
2024-08-28 19:13:36 +0100

    chore: Refactor subscriber.ts to use SQSHandler and handle batch item failures

--- src/subscriber.ts
+++ src/subscriber.ts
@@ -1,11 +1,26 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { SQSBatchItemFailure, SQSBatchResponse, SQSHandler } from "aws-lambda";
 
-export const handler = async (
-  event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
-  console.log(event);
+export const handler: SQSHandler = async (
+  event,
+  context
+): Promise<SQSBatchResponse> => {
+  console.dir(event, { depth: null });
+  console.dir(context, { depth: null });
+
+  const batchItemFailures: SQSBatchItemFailure[] = [];
+
+  for (const record of event.Records) {
+    try {
+      console.log(record.body);
+    } catch (error) {
+      console.error(error);
+      batchItemFailures.push({
+        itemIdentifier: record.messageId,
+      });
+    }
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify("ok"),
+    batchItemFailures,
   };
 };
